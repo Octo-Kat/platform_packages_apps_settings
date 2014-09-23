72ad6d3e698f257d69d1bcae406d780e80d426a7
treken <chupekevin@gmail.com>
2014-09-23 18:55:22 -0500

    Build Tentacles into Settings

--- Android.mk
+++ Android.mk
@@ -6,9 +6,15 @@ LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-v13 jsr305
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := \
-        $(call all-java-files-under, src) \
+LOCAL_SRC_FILES := $(call all-java-files-under, src) \
         src/com/android/settings/EventLogTags.logtags
+LOCAL_SRC_FILES += $(call all-java-files-under, ../Tentacles/src)
+
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DIR += packages/apps/Tentacles/res
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay \
+        --extra-packages com.oct.tentacles \
 
 LOCAL_PACKAGE_NAME := Settings
 LOCAL_CERTIFICATE := platform
@@ -17,6 +23,8 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_AAPT_FLAGS += -c zz_ZZ
+
+LOCAL_JAVA_LIBRARIES += org.cyanogenmod.hardware
 
 include $(BUILD_PACKAGE)
 
